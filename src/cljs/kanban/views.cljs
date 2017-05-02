874848fa2b9a46190ebdcc26ef92503bd924967e
Jakub Dundalek <dundalek@gmail.com>
2017-05-02 12:35:16 +0200

    Checkpoint

--- src/cljs/kanban/views.cljs
+++ src/cljs/kanban/views.cljs
@@ -4,6 +4,22 @@
               [reagent.core :as r]
               [reagent-dnd.core :as dnd]))
 
+(defn render-card [{title :title card-id :id} drag-state]
+  (let [dragging (= card-id (-> @drag-state :item :id))]
+    [:div.kanban-card {:key card-id
+                       :style {:opacity (if dragging 0 1)}}
+                      title]))
+
+(defn render-draggable-card [card drag-state]
+  [dnd/drag-source
+   :type       :card
+   :end-drag   (fn [state]
+                 (dispatch [:move-card (-> state :item :id) (-> state :drop-result :id)])
+                 (js/console.error "dropped on: " (clj->js state)))
+   :begin-drag (fn [] {:id (:id card)})
+   :state      drag-state
+   :child [render-card card drag-state]])
+
 (defn board []
   (let [columns (subscribe [:columns])
         cards (subscribe [:cards])
@@ -14,7 +30,7 @@
         [:div.kanban-board
           (for [{title :title column-id :id} @columns]
               [dnd/drop-target
-               :types [:knight]
+               :types [:card]
                :drop (fn [state]
                        (js/console.error "dropped item: " (clj->js (:item state)))
                        {:id column-id})
@@ -24,16 +40,8 @@
                    {:key title}
                    [:header title]
                    [:section.wrapper.ui-sortable
-                     (for [{title :title card-id :id} (column-cards column-id)]
-                         [dnd/drag-source
-                          :type       :knight
-                          :end-drag   (fn [state]
-                                        (dispatch [:move-card (-> state :item :id) (-> state :drop-result :id)])
-                                        (js/console.error "dropped on: " (clj->js state)))
-                          :begin-drag (fn [] {:id card-id})
-                          :state      drag-state
-                          :child
-                            [:div.kanban-card {:key title :draggable "true"} title]])]
+                     (for [card (column-cards column-id)]
+                       [render-draggable-card card drag-state])]
                    [:footer "Add Card"]]])]))))
 
 (defn main-panel []
