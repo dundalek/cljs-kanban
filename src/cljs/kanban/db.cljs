f8f6a20a1515f8f5c2c933b0b7039006496e485e
Jakub Dundalek <dundalek@gmail.com>
2017-05-15 12:20:46 +0200

    Generate more cards for testing

--- src/cljs/kanban/db.cljs
+++ src/cljs/kanban/db.cljs
@@ -5,10 +5,14 @@
              {:id 2 :title "To Do"}
              {:id 3 :title "In Progress"}
              {:id 4 :title "Done"}]
-   :cards [{:id 1 :title "a" :column-id 1}
-           {:id 2 :title "b" :column-id 1}
-           {:id 6 :title "f" :column-id 1}
-           {:id 7 :title "g" :column-id 1}
-           {:id 3 :title "c" :column-id 2}
-           {:id 4 :title "d" :column-id 2}
-           {:id 5 :title "e" :column-id 3}]})
+   :cards
+    (vec
+      (concat
+        [{:id 1 :title "a" :column-id 1}
+         {:id 2 :title "b" :column-id 1}
+         {:id 6 :title "f" :column-id 1}
+         {:id 7 :title "g" :column-id 1}
+         {:id 3 :title "c" :column-id 2}
+         {:id 4 :title "d" :column-id 2}
+         {:id 5 :title "e" :column-id 3}]))})
+        ;(map (fn [id] {:id id :title (str "card-" id) :column-id 1}) (range 10 30))))})
